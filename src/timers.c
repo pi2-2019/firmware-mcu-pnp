6f28133c8a263bad28acf21f4601145dd8de0b64
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2019-06-30 22:52:56 -0300

    Add timer configuration without interruptions
    Rename the old function in order to make clear that the interruption
    will be enabled.
    
    Add the function `start_t1_a3_c0` with the same behaviour as
    `start_t1_a3_c0_it`, but without launching the maskable interruption.

--- src/timers.c
+++ src/timers.c
@@ -2,7 +2,7 @@
 #include "sys_config.h"
 #include "timers.h"
 
-void start_t1_a3_c0(unsigned int period)
+void start_t1_a3_c0_it(unsigned int period)
 {
 	/* Configure and start Timer1_A3.TA0 to generate pulses
 	 * Stop the clock
@@ -13,6 +13,19 @@ void start_t1_a3_c0(unsigned int period)
 	TA1CCR0 = period;
 	TA1CCR1 = 0;
 	TA1CCTL0 = CCIE;
+	TA1CTL = TASSEL_2 | MC_1 | TACLR;
+}
+
+void start_t1_a3_c0(unsigned int period)
+{
+	/* Configure and start Timer1_A3.TA0 to generate pulses
+	 * Stop the clock
+	 * set interrupts period
+	 * set source as SMCLK (8 MHz, up mode, clear timer control)
+	 */
+	TA1CTL = MC_0;
+	TA1CCR0 = period;
+	TA1CCR1 = 0;
 	TA1CTL = TASSEL_2 | MC_1 | TACLR;
 }
 
